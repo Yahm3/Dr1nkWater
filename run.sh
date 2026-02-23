953390f505b73af849d980391e77cafdb04876dc
yahm3 <240418541@tut4life.ac.za>
2026-02-23 21:40:31 +0200

    build: update run.sh file to use JDK21

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-# Builds and runs the Gradle application.
+
+export JAVA_HOME=/usr/lib/jvm/java-21-openjdk-amd64
+
 echo "Building and running Dr1nkWater..."
+
 ./gradlew build && ./gradlew :app:run
