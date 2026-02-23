ca44c4f185ff481ceb8193d603135227f496e93c
yahm3 <240418541@tut4life.ac.za>
2026-02-23 21:38:22 +0200

    feat: update the test and main file

--- app/src/main/java/com/example/App.java
+++ app/src/main/java/com/example/App.java
@@ -1,4 +1,5 @@
 package com.example;
+
 import java.awt.*;
 import javax.swing.*;
 import java.util.*;
@@ -10,59 +11,61 @@ import javax.swing.Timer;// for ambiguity
 
 /* @author Innocent: https://github.com/Yahm3 **/
 public class App {
-	static	 final class SimpleFrame extends JFrame{
-		private JLabel timerLabel = new JLabel();
-		private int remainingSeconds = 300; // 5 minutes which is the default
-		private Timer timer;
+  static final class SimpleFrame extends JFrame {
+    private JLabel timerLabel = new JLabel();
+    private int remainingSeconds = 300; // 5 minutes which is the default
+    private Timer timer;
 
-		public SimpleFrame(){
-			super("Time to drink water!!");
-			this.setSize(900,500);
-			this.setLocationRelativeTo(null);
-			this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);// To prevent snoozing action or avoiding dehydrating
-			this.setLayout(new BorderLayout());
+    public SimpleFrame() {
+      super("Time to drink water!!");
+      this.setSize(900, 500);
+      this.setLocationRelativeTo(null);
+      this.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);// To prevent snoozing action or avoiding dehydrating
+      this.setLayout(new BorderLayout());
 
-			timerLabel = new JLabel();
-			timerLabel.setFont(new Font("Sans serif", Font.BOLD,50));// you can customize this to your own liking
-			timerLabel.setHorizontalAlignment(JLabel.CENTER);
+      timerLabel = new JLabel();
+      timerLabel.setFont(new Font("Sans serif", Font.BOLD, 50));// you can customize this to your own liking
+      timerLabel.setHorizontalAlignment(JLabel.CENTER);
 
-			// handling when the user wants to snooze, which they cannot haha :)
-			this.addWindowListener(new WindowAdapter(){
-				@Override
-				public void windowClosing(WindowEvent e) {
-					JOptionPane.showMessageDialog(SimpleFrame.this,
-							"This timer cannot be interrupted. Please take a moment to rest and stretch!",
-							"Attention!",
-							JOptionPane.WARNING_MESSAGE);
-				}	
-			});
-			timer = new Timer(1000, new ActionListener(){
-				@Override
-				public void actionPerformed(ActionEvent e){
-					remainingSeconds--;	
-					int minutes = remainingSeconds / 60;
-					int seconds  = remainingSeconds % 60;
-					timerLabel.setText(String.format("%2d:%02d", minutes, seconds));
+      // handling when the user wants to snooze, which they cannot haha :)
+      this.addWindowListener(new WindowAdapter() {
+        @Override
+        public void windowClosing(WindowEvent e) {
+          JOptionPane.showMessageDialog(SimpleFrame.this,
+              "This timer cannot be interrupted. Please take a moment to rest and stretch!",
+              "Attention!",
+              JOptionPane.WARNING_MESSAGE);
+        }
+      });
+      timer = new Timer(1000, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          remainingSeconds--;
+          int minutes = remainingSeconds / 60;
+          int seconds = remainingSeconds % 60;
+          timerLabel.setText(String.format("%2d:%02d", minutes, seconds));
 
-					if(remainingSeconds <= 60){
-						timerLabel.setForeground(Color.RED);	// To show emegence 
-					}else{
-						timerLabel.setForeground(Color.BLACK);	// To show emegence 
-					}
-					if(remainingSeconds <= 0){
-						timer.stop();	
-						Toolkit.getDefaultToolkit().beep();
-						SimpleFrame.this.dispose();// after dehydration time dispose the frame
-					}
-				}	
-			});
-			timer.start();
-			this.add(timerLabel,BorderLayout.CENTER);
-			this.setVisible(true);
-		}// "It's so lonely out here" ~ raysan5
-	}
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(() -> {new SimpleFrame();
-		});
-	}
+          if (remainingSeconds <= 60) {
+            timerLabel.setForeground(Color.RED); // To show emegence
+          } else {
+            timerLabel.setForeground(Color.BLACK); // To show emegence
+          }
+          if (remainingSeconds <= 0) {
+            timer.stop();
+            Toolkit.getDefaultToolkit().beep();
+            SimpleFrame.this.dispose();// after dehydration time dispose the frame
+          }
+        }
+      });
+      timer.start();
+      this.add(timerLabel, BorderLayout.CENTER);
+      this.setVisible(true);
+    }// "It's so lonely out here" ~ raysan5
+  }
+
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(() -> {
+      new SimpleFrame();
+    });
+  }
 }
